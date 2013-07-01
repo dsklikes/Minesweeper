3a45c0253c105008e1efe465774b90d187018cfd
David Kim <dasaki@gmail.com>
2013-07-01 15:02:02 -0700

    some game logic

---
+++ gamelogic.js
@@ -0,0 +1,20 @@
+
+function createBoard(size){
+	var rowsize = size;
+	var colsize = size;
+	for (var x=0;i<rowsize;x++)
+	{
+		for (var y=0;i<colsize;y++)
+		{
+			tile[x][y].hasMine = false;
+			tile[x][y].isClicked = false;
+		}
+	}
+}
+
+
+$('td').click(function(){
+  var col = $(this).parent().children().index($(this));
+  var row = $(this).parent().parent().children().index($(this).parent());
+  
+});
